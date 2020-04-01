e1f089927fc531d530305f5b3aa0aafbff3ce687
iTeller_zc <18060903@cnsuning.com>
2020-04-01 09:10:40 +0800

    add map test

---
+++ src/main/java/com/iteller/kl/java/util/collection/map/MyHashMap.java
@@ -0,0 +1,18 @@
+package com.iteller.kl.java.util.collection.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: 18060903(iTeller_zc)
+ * date:2020/3/31 16:03
+ * description:
+ */
+public class MyHashMap {
+
+    public static void main(String[] args){
+
+        Map<String, String> map = new HashMap<String, String>(10);
+        System.out.println(map.size());
+    }
+}
