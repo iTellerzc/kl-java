3873a71ccd33878b1df7681a65e72ebabc552703
iTeller_zc <18060903@cnsuning.com>
2020-04-07 11:21:58 +0800

    添加countdownlatch

---
+++ src/main/java/com/iteller/kl/java/util/concurrent/countdownlatch/CountDownLatchTaskUtils.java
@@ -0,0 +1,94 @@
+package com.iteller.kl.java.util.concurrent.countdownlatch;
+
+import org.apache.commons.collections.CollectionUtils;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author: 18060903(iTeller_zc)
+ * date:2020/4/7 10:54
+ * description:
+ */
+public class CountDownLatchTaskUtils {
+
+    public static final int POOL_SIZE ;
+
+    static {
+        //System.out.println(Runtime.getRuntime().availableProcessors() + "个可用处理器");
+        POOL_SIZE = Integer.max(Runtime.getRuntime().availableProcessors(), 5);
+    }
+
+
+    /**
+     * 并行处理任务,等待结束
+     * @param taskLists
+     * @param consumer
+     * @param <T>
+     */
+    public static <T> void dispose(List<T> taskLists, Consumer<T> consumer) throws InterruptedException {
+        dispose(true, POOL_SIZE, taskLists, consumer);
+    }
+
+    /**
+     * 线程池处理并等待结束
+     * @param moreThread
+     * @param poolSize
+     * @param taskLists
+     * @param consumer
+     * @param <T>
+     */
+    private static <T> void dispose(boolean moreThread, int poolSize, List<T> taskLists, Consumer<T> consumer) throws InterruptedException {
+        if(CollectionUtils.isEmpty(taskLists)){
+            return;
+        }
+
+        if(moreThread && poolSize > 1){
+            poolSize = Integer.min(poolSize, taskLists.size());
+            ExecutorService executorService = null;
+            try{
+                executorService =  Executors.newFixedThreadPool(poolSize);
+                CountDownLatch countDownLatch = new CountDownLatch(taskLists.size());
+                for(T item : taskLists){
+                    executorService.execute(()-> {
+                        try{
+                            consumer.accept(item);
+                        }finally {
+                            countDownLatch.countDown();
+                        }
+                    });
+                }
+                countDownLatch.await();
+            }finally {
+                if(executorService != null){
+                    executorService.shutdown();
+                }
+            }
+        }else{
+            for(T item : taskLists){
+                consumer.accept(item);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        List<Integer> list = Stream.iterate(1, a->a+1).limit(10).collect(Collectors.toList());
+        CountDownLatchTaskUtils.dispose(list, item->{
+            try {
+                long startTime = System.currentTimeMillis();
+                TimeUnit.SECONDS.sleep(item);
+                long endTime = System.currentTimeMillis();
+                System.out.println(endTime + ", 任务：" + item + "执行完毕,耗时:" + (endTime-startTime) + " ms");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        System.out.println(list + "中的所有任务都执行完毕");
+    }
+}
---
+++ src/main/java/com/iteller/kl/java/util/concurrent/countdownlatch/CountDownLatchWithWaitTime.java
@@ -0,0 +1,65 @@
+package com.iteller.kl.java.util.concurrent.countdownlatch;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author: 18060903(iTeller_zc)
+ * date:2020/4/7 10:15
+ * description:
+ *
+ * 创建CountDownLatch对象
+ * 调用其实例方法 await()，让当前线程等待
+ * 调用 countDown()方法，让计数器减1
+ * 当计数器变为0的时候， await()方法会返回
+ */
+public class CountDownLatchWithWaitTime {
+
+    public static class T extends Thread{
+
+        int sleepSeconds;
+        CountDownLatch countDownLatch;
+
+        public T(String name, int sleepSeconds, CountDownLatch countDownLatch){
+            super(name);
+            this.sleepSeconds = sleepSeconds;
+            this.countDownLatch = countDownLatch;
+        }
+
+        @Override
+        public void run(){
+            Thread currentThread = currentThread();
+            long startTime = System.currentTimeMillis();
+            System.out.println(startTime+", " + currentThread.getName() + "开始处理");
+            try{
+                TimeUnit.SECONDS.sleep(sleepSeconds);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }finally {
+                countDownLatch.countDown();
+            }
+            long endTime = System.currentTimeMillis();
+            System.out.println(endTime+", " + currentThread.getName() + "处理结束,耗时:" + (endTime - startTime) + " ms");
+
+        }
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(System.currentTimeMillis() + "," + Thread.currentThread().getName() + " 线程start");
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+
+        long startTime = System.currentTimeMillis();
+        T t1 = new T("t1", 2, countDownLatch);
+        t1.start();
+
+        T t2 = new T("t2", 5, countDownLatch);
+        t2.start();
+
+        boolean flag = countDownLatch.await(6, TimeUnit.SECONDS);
+        System.out.println(System.currentTimeMillis() + "," + Thread.currentThread().getName() + " 线程end");
+        long endTime = System.currentTimeMillis();
+        System.out.println("总耗时:" + (endTime-startTime) + " ms, result:" + flag);
+
+    }
+}
