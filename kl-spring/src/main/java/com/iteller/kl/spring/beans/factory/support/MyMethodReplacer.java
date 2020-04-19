fcebe376b0d1e06c8f2a2a33936b431b5aedc2b8
iTeller_zc <iTeller_zc@163.com>
2020-04-19 16:09:21 +0800

    method invoke replace

---
+++ kl-spring/src/main/java/com/iteller/kl/spring/beans/factory/support/MyMethodReplacer.java
@@ -0,0 +1,20 @@
+package com.iteller.kl.spring.beans.factory.support;
+
+import org.springframework.beans.factory.support.MethodReplacer;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author iTeller_zc
+ * date:2020/4/19
+ * description:
+ */
+public class MyMethodReplacer implements MethodReplacer {
+
+    @Override
+    public Object reimplement(Object obj, Method method, Object[] args) throws Throwable {
+        System.out.println("this is real invoke!");
+        String input = (String) args[0];
+        return input;
+    }
+}
---
+++ kl-spring/src/main/java/com/iteller/kl/spring/service/replace/MyReplacer.java
@@ -0,0 +1,13 @@
+package com.iteller.kl.spring.service.replace;
+
+/**
+ * @author iTeller_zc
+ * date:2020/4/19
+ * description:
+ */
+public class MyReplacer {
+
+    public String replace(String input){
+        return "use replace instead of:" + input;
+    }
+}
