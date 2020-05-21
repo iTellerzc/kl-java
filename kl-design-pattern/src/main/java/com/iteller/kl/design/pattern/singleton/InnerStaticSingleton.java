c2200c276069f1ccd93d5b667e42e112c2182e03
iTeller_zc <18060903@cnsuning.com>
2020-05-21 15:28:27 +0800

    添加单例模式

---
+++ kl-design-pattern/src/main/java/com/iteller/kl/design/pattern/singleton/InnerStaticSingleton.java
@@ -0,0 +1,22 @@
+package com.iteller.kl.design.pattern.singleton;
+
+/**
+ * @author: 18060903(iTeller_zc)
+ * date:2020/5/21 15:06
+ * description:
+ */
+public class InnerStaticSingleton {
+
+    private InnerStaticSingleton(){
+
+    }
+
+    private static class InnerSingleton{
+        private static InnerStaticSingleton instance = new InnerStaticSingleton();
+    }
+
+    public static InnerStaticSingleton getInstance(){
+        return InnerSingleton.instance;
+    }
+
+}
