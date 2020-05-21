4839f7665bc8d6fe6330fcf05e154873a0e15094
iTeller_zc <18060903@cnsuning.com>
2020-05-21 17:52:33 +0800

    添加io

---
+++ kl-netty/src/main/java/com/iteller/kl/io/bio/client/Client.java
@@ -0,0 +1,13 @@
+package com.iteller.kl.io.bio.client;
+
+/**
+ * @author: 18060903(iTeller_zc)
+ * date:2020/5/21 17:29
+ * description:
+ */
+public class Client {
+
+    public static void main(String[] args){
+
+    }
+}
