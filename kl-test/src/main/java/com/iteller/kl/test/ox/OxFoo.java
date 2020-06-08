747245e4ac7961830a9231dbef9b6398ee150af7
iTeller_zc <18060903@cnsuning.com>
2020-06-08 11:06:58 +0800

    16进制测试

---
+++ kl-test/src/main/java/com/iteller/kl/test/ox/OxFoo.java
@@ -0,0 +1,13 @@
+package com.iteller.kl.test.ox;
+
+/**
+ * @author: 18060903(iTeller_zc)
+ * date:2020/6/8 9:19
+ * description:
+ */
+public class OxFoo {
+
+    public static void main(String[] args){
+        System.out.println(0x78);
+    }
+}
