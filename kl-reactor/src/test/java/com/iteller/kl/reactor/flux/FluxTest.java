b8e90560d06d819b84996d07f21790673e99293b
iTeller_zc <iTeller_zc@163.com>
2020-08-01 13:06:52 +0800

    添加flux测试

--- kl-reactor/src/test/java/com/iteller/kl/reactor/flux/FluxTest.java
+++ kl-reactor/src/test/java/com/iteller/kl/reactor/flux/FluxTest.java
@@ -3,6 +3,9 @@ package com.iteller.kl.reactor.flux;
 import org.testng.annotations.Test;
 import reactor.core.publisher.Flux;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author iTeller_zc (zhangchao@elextec.com)
  * @date 2020/7/29 17:02
@@ -55,5 +58,16 @@ public class FluxTest {
                 error -> System.out.println(error),
                 () -> System.out.println("done"),
                 sub -> sub.request(10));
+    }
+
+    @Test
+    public void testFromIterable(){
+       Flux<String> seq1 =  Flux.just("a", "b", "c");
+       System.out.println(seq1.blockFirst());
+       System.out.println(seq1);
+       List<String> iterable = Arrays.asList("a", "b", "c");
+       Flux<String> seq2 = Flux.fromIterable(iterable);
+       System.out.println(seq2.blockFirst());
+       System.out.println(seq2);
     }
 }
